9a5745b094f8620d0585cdc6f4c345e30c35540e
DeanCaly <deancaly@gmail.com>
2025-09-07 23:37:03 -0300

    feat: adição de rota para função DELETE, e algumas correções

--- app.py
+++ app.py
@@ -3,24 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contatos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicialização do SQLAlchemy com a aplicação Flask.
 db = SQLAlchemy(app)
 
-class Contato(db.model):
-    id = db.Column(db.Integer, primary_Key=True)
-    nome = db.Column(db.String(20), nullable=False)
-    telefone = db.Column(db.Integer, nullable=True)
+class Contato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(40), nullable=False)
+    telefone = db.Column(db.String(20), nullable=True)
 
-
-    def toDict(self):
+    def __repr__(self):
+        return f'Contato {self.nome}'
+    
+    def to_dict(self):
         return {
             'id': self.id,
             'nome': self.nome,
-            'fone': self.telefone
+            'telefone': self.telefone
         }
     
 
@@ -29,20 +31,20 @@ with app.app_context():
 
 #Rotas da API
 
-@app.route('/contatos', methods=["GET", "POST"])
-
+@app.route('/contatos', methods=['GET', 'POST'])
 def handle_contatos():
-    if request.method == "GET":
+    if request.method == 'GET':
         contatos = Contato.query.all()
-        return jsonify([contato.toDict() for contato in contatos])
-    elif request.method == "POST":
+        return jsonify([contato.to_dict() for contato in contatos])
+    
+    elif request.method == 'POST':
         data = request.get_json()
 
         #validação
-        if not data or not 'nome' in data:
+        if not data or 'nome' not in data:
             return jsonify({'message': 'Nome do contato é obrigatório!'}), 400
         
-        novo_contato = Contato(nome=data.get('nome'), telefone=data.get('telefone'))
+        novo_contato = Contato(nome=data['nome'], telefone=data.get('telefone'))
 
          # Adiciona o novo contato à sessão do banco de dados.
         db.session.add(novo_contato)
@@ -50,6 +52,20 @@ def handle_contatos():
         # Salva as alterações no banco de dados.
         db.session.commit()
 
-        return jsonify(novo_contato.toDict())
-    
+        return jsonify(novo_contato.to_dict()), 201 
 
+
+@app.route('/contatos/<int:contato_id>', methods=['DELETE'])
+def delete_contato(contato_id):
+    contato = Contato.query.get_or_404(contato_id)
+
+    db.session.delete(contato)
+    db.session.commit()
+
+    return "", 204
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
